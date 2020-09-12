8fbf5b5bc1242a5f841211013089b494c9479e2a
gilang-novrizal <gilnov.201196@gmail.com>
2020-09-12 12:34:53 +0700

    setup express

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("test")
+const express = require('express')
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const dotenv = require('dotenv')
+
+const app = express()
+dotenv.config()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res)=>{
+    res.status(200).send("Final Project API")
+})
+
+const database = require('./database')
+database.connect((err)=>{
+    if(err){
+        return console.error("error connecting : " + err.stack)
+    }
+
+    console.log("Connected as id : ", database.threadId)
+})
+
+const PORT = 3000
+app.listen(PORT, ()=> console.log(`Server is running at port ${PORT}`))
