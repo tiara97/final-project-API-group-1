f3c3ac2a88532e3a2fe5cbc9c3b1b927efdad585
tiara97 <tiara.hndt@gmail.com>
2020-09-16 17:04:12 +0700

    edit change

--- database/index.js
+++ database/index.js
@@ -1,9 +1,6 @@
 const mysql = require('mysql')
 
-<<<<<<< HEAD
-=======
 // connect to mysql database
->>>>>>> 8e0f3a20723367c31c58c45e31b02db6531c5496
 const database = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
--- router/index.js
+++ router/index.js
@@ -4,5 +4,7 @@ const profileRouter = require("./profileRouter")
 const carouselRouter = require("./carouselRouter")
 const transactionRouter = require('./transactionRouter')
 const warehouseRouter = require('./warehouseRouter')
+const categoryRouter = require("./categoryRouter")
 
-module.exports = {userRouter, orderRouter, profileRouter, carouselRouter, transactionRouter, warehouseRouter}
+module.exports = {userRouter, orderRouter, profileRouter, carouselRouter, categoryRouter, transactionRouter, warehouseRouter}
+
