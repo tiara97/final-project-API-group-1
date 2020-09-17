c2dadb9e777e7b14d4ce423ead09e9d4963d5762
Muhammadkpn <muhammadkpn@gmail.com>
2020-09-17 20:17:12 +0700

    edit cart and product

--- router/cartRouter.js
+++ router/cartRouter.js
@@ -2,13 +2,13 @@
 const router = require('express').Router()
 
 // import controller
-const { cartRouter } = require('../controller')
+const { cartController } = require('../controller')
 
 // create route
-router.get('/cart/:id', cartRouter.getCart)
-router.post('/cart/add', cartRouter.addToCart)
-router.patch('/cart/edit/:id', cartRouter.editQtyInCart)
-router.delete('/cart/delete/:id', cartRouter.deleteCart)
+router.get('/cart/:id', cartController.getCart)
+router.post('/cart/add', cartController.addToCart)
+router.patch('/cart/edit/:id', cartController.editQtyInCart)
+router.delete('/cart/delete/:id', cartController.deleteCart)
 
 // export router
 module.exports = router
