587a76eb93d807063176caadcf8eebf967ae6631
gilang-novrizal <gilnov.201196@gmail.com>
2020-09-19 11:04:49 +0700

    edit warehouse controller

--- router/cartRouter.js
+++ router/cartRouter.js
@@ -9,6 +9,7 @@ router.get('/cart/:id', cartController.getCart)
 router.post('/cart/add', cartController.addToCart)
 router.patch('/cart/edit/:id', cartController.editQtyInCart)
 router.delete('/cart/delete/:id', cartController.deleteCart)
+router.patch('/cart/warehouseID/:id', cartController.updateWarehouseID)
 
 // export router
 module.exports = router
