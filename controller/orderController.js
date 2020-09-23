c95de3c88007e33fc131583c6f8dd116ff84b44c
tiara97 <tiara.hndt@gmail.com>
2020-09-23 20:36:41 +0700

    edit query order

--- controller/orderController.js
+++ controller/orderController.js
@@ -6,15 +6,17 @@ module.exports = {
         try {
             // get all order data
             const getOrders = `SELECT o.id, o.user_id, o.order_number, o.order_date, o.required_date,
-            o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, GROUP_CONCAT(pi.image) AS image,
+            o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, GROUP_CONCAT(tb2.image) AS image,
             GROUP_CONCAT(od.qty) AS qty, GROUP_CONCAT(od.price_each) AS price_each, os.status, w.name as warehouse  FROM orders o
             JOIN order_details od ON o.order_number = od.order_number
             JOIN order_status os ON o.order_status_id = os.id
             JOIN product_color pc ON od.color_id = pc.id
-            JOIN product_images pi ON od.product_id = pi.product_id
             JOIN warehouse w ON o.warehouse_id = w.id
             JOIN products p ON p.id = od.product_id
-            GROUP BY o.order_number;`
+            JOIN (SELECT product_id, image
+                              FROM product_images
+                              GROUP BY product_id) AS tb2 ON od.product_id = tb2.product_id
+            GROUP BY o.order_number ORDER BY o.order_date;`
             const result = await asyncQuery(getOrders)
 
             const getTotal = `SELECT SUM(price_each * qty) AS total FROM order_details GROUP BY ORDER_NUMBER`
@@ -48,9 +50,9 @@ module.exports = {
             JOIN warehouse w ON o.warehouse_id = w.id
             JOIN products p ON p.id = od.product_id
             JOIN (SELECT product_id, image
-                                          FROM product_images
-                                          GROUP BY product_id) AS tb2 ON od.product_id = tb2.product_id
-            WHERE o.user_id = ${database.escape(Id)} GROUP BY o.order_number;`
+                FROM product_images
+                GROUP BY product_id) AS tb2 ON od.product_id = tb2.product_id
+            WHERE o.user_id = ${database.escape(Id)} GROUP BY o.order_number ORDER BY o.order_date;`
             const result = await asyncQuery(getOrders)
 
             const getTotal = `SELECT SUM(price_each * qty) AS total FROM order_details od
@@ -76,16 +78,18 @@ module.exports = {
         try {
             // get order per user id
             const getOrders = `SELECT o.id, o.user_id, o.order_number, o.order_date, o.required_date,
-            o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, GROUP_CONCAT(pi.image) AS image,
-            GROUP_CONCAT(od.qty) AS qty, GROUP_CONCAT(od.price_each) AS price_each, os.status, w.name as warehouse  FROM orders o
+            o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, 
+            GROUP_CONCAT(tb2.image) AS image, GROUP_CONCAT(od.qty) AS qty, GROUP_CONCAT(od.price_each) AS price_each, 
+            os.status, w.name as warehouse  FROM orders o
             JOIN order_details od ON o.order_number = od.order_number
             JOIN order_status os ON o.order_status_id = os.id
-            JOIN warehouse w ON o.warehouse_id = w.id
-            JOIN product_images pi ON od.product_id = pi.product_id
             JOIN product_color pc ON od.color_id = pc.id
+            JOIN warehouse w ON o.warehouse_id = w.id
             JOIN products p ON p.id = od.product_id
-            WHERE o.order_number = ${order_number}
-            GROUP BY o.order_number;`
+            JOIN (SELECT product_id, image
+                FROM product_images
+                GROUP BY product_id) AS tb2 ON od.product_id = tb2.product_id
+            WHERE o.order_number = ${database.escape(order_number)} GROUP BY o.order_number;`
             const result = await asyncQuery(getOrders)
 
             result.forEach((item)=>{
@@ -111,14 +115,70 @@ module.exports = {
         try {
             // get order per warehouse
             const getOrders = `SELECT o.id, o.user_id, o.order_number, o.order_date, o.required_date,
-            o.send_date, o.done_date , od.product_id, pc.color, od.qty, 
-            od.price_each, os.status, w.name as warehouse  FROM orders o
+            o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, 
+            GROUP_CONCAT(tb2.image) AS image, GROUP_CONCAT(od.qty) AS qty, GROUP_CONCAT(od.price_each) AS price_each, 
+            os.status, w.name as warehouse  FROM orders o
             JOIN order_details od ON o.order_number = od.order_number
             JOIN order_status os ON o.order_status_id = os.id
             JOIN product_color pc ON od.color_id = pc.id
             JOIN warehouse w ON o.warehouse_id = w.id
-            WHERE w.id = ${database.escape(Id)};`
+            JOIN products p ON p.id = od.product_id
+            JOIN (SELECT product_id, image
+                FROM product_images
+                GROUP BY product_id) AS tb2 ON od.product_id = tb2.product_id
+            WHERE o.warehouse_id = ${database.escape(Id)} GROUP BY o.order_number ORDER BY o.order_date;`
             const result = await asyncQuery(getOrders)
+
+            const getTotal = `SELECT SUM(price_each * qty) AS total FROM order_details od
+            JOIN orders o ON o.order_number = od.order_number
+             WHERE o.warehouse_id = ${database.escape(Id)} GROUP BY od.order_number;`
+            const total = await asyncQuery(getTotal)
+            result.forEach((item, index)=>{
+                item.name = item.name.split(",")
+                item.color = item.color.split(",")
+                item.qty = item.qty.split(",")
+                item.image = item.image.split(",")
+                item.price_each = item.price_each.split(",")
+                item.total = total[index].total
+                })
+            res.status(200).send(result) 
+        } catch (error) {
+            console.log(error)
+            res.status(500).send(error)
+        }
+    },
+    // all order -> UNTUK ADMIN
+    getOrdersByStatus: async(req,res)=>{
+        // id status order 
+        const Id = req.params.id
+        try {
+            // get order per warehouse
+            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.order_date, o.required_date,
+            o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, 
+            GROUP_CONCAT(tb2.image) AS image, GROUP_CONCAT(od.qty) AS qty, GROUP_CONCAT(od.price_each) AS price_each, 
+            os.status, w.name as warehouse  FROM orders o
+            JOIN order_details od ON o.order_number = od.order_number
+            JOIN order_status os ON o.order_status_id = os.id
+            JOIN product_color pc ON od.color_id = pc.id
+            JOIN warehouse w ON o.warehouse_id = w.id
+            JOIN products p ON p.id = od.product_id
+            JOIN (SELECT product_id, image FROM product_images
+                    GROUP BY product_id) AS tb2 ON od.product_id = tb2.product_id
+            WHERE o.order_status_id = ${database.escape(Id)} GROUP BY o.order_number ORDER BY o.order_date;`
+            const result = await asyncQuery(getOrders)
+
+            const getTotal = `SELECT SUM(price_each * qty) AS total FROM order_details od
+            JOIN orders o ON o.order_number = od.order_number
+             WHERE o.order_status_id = ${database.escape(Id)} GROUP BY od.order_number;`
+            const total = await asyncQuery(getTotal)
+            result.forEach((item, index)=>{
+                item.name = item.name.split(",")
+                item.color = item.color.split(",")
+                item.qty = item.qty.split(",")
+                item.image = item.image.split(",")
+                item.price_each = item.price_each.split(",")
+                item.total = total[index].total
+                })
             res.status(200).send(result) 
         } catch (error) {
             console.log(error)
--- router/orderRouter.js
+++ router/orderRouter.js
@@ -7,5 +7,6 @@ router.get("/get", orderController.getOrdersData)
 router.get("/getByUserID/:id", orderController.getOrdersById)
 router.get("/getByOrderNumber/:order_number", orderController.getOrdersByNumber)
 router.get("/getByWarehouseID/:id", orderController.getOrdersByWarehouse)
+router.get("/getByOrderStatus/:id", orderController.getOrdersByStatus)
 
 module.exports= router
