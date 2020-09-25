fc34805cb7e40f5680b3a5fd4d54dd998fd8e80e
tiara97 <tiara.hndt@gmail.com>
2020-09-25 19:27:50 +0700

    add reject con, edit order con

--- controller/orderController.js
+++ controller/orderController.js
@@ -5,7 +5,7 @@ module.exports = {
     getOrdersData: async(req,res)=>{
         try {
             // get all order data
-            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.order_date, o.required_date,
+            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.total_ongkir, o.payment_note, o.upload_date, o.order_date, o.required_date,
             o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, GROUP_CONCAT(tb2.image) AS image,
             GROUP_CONCAT(od.qty) AS qty, GROUP_CONCAT(od.price_each) AS price_each, os.status, w.name as warehouse  FROM orders o
             JOIN order_details od ON o.order_number = od.order_number
@@ -19,7 +19,9 @@ module.exports = {
             GROUP BY o.order_number ORDER BY o.order_date;`
             const result = await asyncQuery(getOrders)
 
-            const getTotal = `SELECT SUM(price_each * qty) AS total FROM order_details GROUP BY ORDER_NUMBER`
+            const getTotal = `SELECT SUM(price_each * qty) AS total FROM order_details od
+            JOIN orders o ON o.order_number = od.order_number
+            GROUP BY od.order_number ORDER BY o.order_date;`
             const total = await asyncQuery(getTotal)
             result.forEach((item, index)=>{
                 item.name = item.name.split(",")
@@ -40,7 +42,7 @@ module.exports = {
         const Id = req.params.id
         try {
             // get order per user id
-            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.order_date, o.required_date,
+            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.total_ongkir, o.payment_note, o.upload_date, o.order_date, o.required_date,
             o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, 
             GROUP_CONCAT(tb2.image) AS image, GROUP_CONCAT(od.qty) AS qty, GROUP_CONCAT(od.price_each) AS price_each, 
             os.status, w.name as warehouse  FROM orders o
@@ -57,7 +59,7 @@ module.exports = {
 
             const getTotal = `SELECT SUM(price_each * qty) AS total FROM order_details od
             JOIN orders o ON o.order_number = od.order_number
-             WHERE o.user_id = ${database.escape(Id)} GROUP BY od.order_number;`
+             WHERE o.user_id = ${database.escape(Id)} GROUP BY od.order_number ORDER BY o.order_date;`
             const total = await asyncQuery(getTotal)
             result.forEach((item, index)=>{
                 item.name = item.name.split(",")
@@ -77,7 +79,7 @@ module.exports = {
         const order_number = req.params.order_number
         try {
             // get order per user id
-            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.order_date, o.required_date,
+            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.total_ongkir, o.payment_note, o.upload_date, o.order_date, o.required_date,
             o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, 
             GROUP_CONCAT(tb2.image) AS image, GROUP_CONCAT(od.qty) AS qty, GROUP_CONCAT(od.price_each) AS price_each, 
             os.status, w.name as warehouse  FROM orders o
@@ -111,10 +113,11 @@ module.exports = {
         }
     },
     getOrdersByWarehouse: async(req,res)=>{
+        // Id warehouse
         const Id = req.params.id
         try {
             // get order per warehouse
-            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.order_date, o.required_date,
+            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.total_ongkir, o.payment_note, o.upload_date, o.order_date, o.required_date,
             o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, 
             GROUP_CONCAT(tb2.image) AS image, GROUP_CONCAT(od.qty) AS qty, GROUP_CONCAT(od.price_each) AS price_each, 
             os.status, w.name as warehouse  FROM orders o
@@ -131,7 +134,7 @@ module.exports = {
 
             const getTotal = `SELECT SUM(price_each * qty) AS total FROM order_details od
             JOIN orders o ON o.order_number = od.order_number
-             WHERE o.warehouse_id = ${database.escape(Id)} GROUP BY od.order_number;`
+             WHERE o.warehouse_id = ${database.escape(Id)} GROUP BY od.order_number ORDER BY o.order_date;`
             const total = await asyncQuery(getTotal)
             result.forEach((item, index)=>{
                 item.name = item.name.split(",")
@@ -153,7 +156,7 @@ module.exports = {
         const Id = req.params.id
         try {
             // get order per warehouse
-            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.order_date, o.required_date,
+            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.total_ongkir, o.payment_note, o.upload_date, o.order_date, o.required_date,
             o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, 
             GROUP_CONCAT(tb2.image) AS image, GROUP_CONCAT(od.qty) AS qty, GROUP_CONCAT(od.price_each) AS price_each, 
             os.status, w.name as warehouse  FROM orders o
@@ -169,7 +172,47 @@ module.exports = {
 
             const getTotal = `SELECT SUM(price_each * qty) AS total FROM order_details od
             JOIN orders o ON o.order_number = od.order_number
-             WHERE o.order_status_id = ${database.escape(Id)} GROUP BY od.order_number;`
+             WHERE o.order_status_id = ${database.escape(Id)} GROUP BY od.order_number ORDER BY o.order_date;`
+            const total = await asyncQuery(getTotal)
+            result.forEach((item, index)=>{
+                item.name = item.name.split(",")
+                item.color = item.color.split(",")
+                item.qty = item.qty.split(",")
+                item.image = item.image.split(",")
+                item.price_each = item.price_each.split(",")
+                item.total = total[index].total
+                })
+            res.status(200).send(result) 
+        } catch (error) {
+            console.log(error)
+            res.status(500).send(error)
+        }
+    },
+     // order per id -> UNTUK USER
+     getUserOrdersByStatus: async(req,res)=>{
+        // id status order 
+        const Id = req.params.id
+        const {order_status_id} = req.body
+        try {
+            // get order per user id and status
+            const getOrders = `SELECT o.id, o.user_id, o.order_number, o.total_ongkir, o.payment_note, o.upload_date, o.order_date, o.required_date,
+            o.send_date, o.done_date , GROUP_CONCAT(p.name) AS name, GROUP_CONCAT(pc.color) AS color, 
+            GROUP_CONCAT(tb2.image) AS image, GROUP_CONCAT(od.qty) AS qty, GROUP_CONCAT(od.price_each) AS price_each, 
+            os.status, w.name as warehouse  FROM orders o
+            JOIN order_details od ON o.order_number = od.order_number
+            JOIN order_status os ON o.order_status_id = os.id
+            JOIN product_color pc ON od.color_id = pc.id
+            JOIN warehouse w ON o.warehouse_id = w.id
+            JOIN products p ON p.id = od.product_id
+            JOIN (SELECT product_id, image
+                FROM product_images
+                GROUP BY product_id) AS tb2 ON od.product_id = tb2.product_id
+            WHERE o.user_id = ${database.escape(Id)} AND o.order_status_id= ${database.escape(order_status_id)} GROUP BY o.order_number ORDER BY o.order_date;`
+            const result = await asyncQuery(getOrders)
+
+            const getTotal = `SELECT SUM(price_each * qty) AS total FROM order_details od
+            JOIN orders o ON o.order_number = od.order_number
+             WHERE o.user_id = ${database.escape(Id)} AND o.order_status_id = ${database.escape(order_status_id)} GROUP BY od.order_number ORDER BY o.order_date;`
             const total = await asyncQuery(getTotal)
             result.forEach((item, index)=>{
                 item.name = item.name.split(",")
