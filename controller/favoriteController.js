76e4ac10c86a8dbcff8a25a0f9dfca447f7a96f5
gilang-novrizal <gilnov.201196@gmail.com>
2020-09-27 21:36:53 +0700

    cleaning

--- controller/favoriteController.js
+++ controller/favoriteController.js
@@ -6,7 +6,7 @@ module.exports= {
     getFavoriteAll: async(req,res)=>{
         try {
             // get favorite data
-            const getFavorite = `SELECT f.id, f.user_id, p.name, pc.color, f.qty, f.price_each, pi.image FROM favorite f 
+            const getFavorite = `SELECT f.id, f.user_id, f.product_id, p.name, pc.color, f.qty, f.price_each, pi.image FROM favorite f 
             JOIN products p ON f.product_id = p.id
             JOIN product_color pc ON f.color_id = pc.id
             JOIN product_images pi ON f.product_id = pi.product_id GROUP BY f.product_id;`
@@ -21,7 +21,7 @@ module.exports= {
         const Id = parseInt(req.params.id)
         try {
             // get favorite data
-            const getFavorite = `SELECT f.id, f.user_id, p.name, pc.color, p.desc, f.qty, f.price_each, pi.image FROM favorite f 
+            const getFavorite = `SELECT f.id, f.user_id, f.product_id, p.name, pc.color, p.desc, f.qty, f.price_each, pi.image FROM favorite f 
             JOIN products p ON f.product_id = p.id
             JOIN product_color pc ON f.color_id = pc.id
             JOIN product_images pi ON f.product_id = pi.product_id WHERE f.user_id = ${database.escape(Id)}
